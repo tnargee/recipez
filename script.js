6df166d30bd675fe30e5d7c24e4fb268f8fc210f
Tenzin Nargee <tenzin.n@Tenzins-MacBook-Pro.local>
2023-11-25 20:31:41 -0500

    Added recipe information and styling

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('recipe-form');
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', async function(event) {
         event.preventDefault();
         const ingredientsInput = document.getElementById('ingredients');
         const ingredients = ingredientsInput.value;
 
-        // Simple validation to check if the input is not empty
         if (!ingredients.trim()) {
             alert('Please enter some ingredients.');
             return;
         }
 
-        const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${encodeURIComponent(ingredients)}&number=5&ignorePantry=true&ranking=1`;
-        const options = {
+        const searchUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${encodeURIComponent(ingredients)}&number=5&ignorePantry=true&ranking=1`;
+        const searchOptions = {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': '6097e393dcmshadee9498b4bc4dbp1772ecjsn591cca02485f',
@@ -20,41 +19,45 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         };
 
-        fetch(url, options)
-            .then(response => response.json())
-            .then(recipes => {
-                displayRecipes(recipes);
-            })
-            .catch(error => {
-                console.error('Error fetching recipes:', error);
-            });
+        try {
+            const searchResponse = await fetch(searchUrl, searchOptions);
+            const recipes = await searchResponse.json();
+            await displayRecipes(recipes);
+        } catch (error) {
+            console.error('Error fetching recipes:', error);
+        }
     });
 });
 
 async function displayRecipes(recipes) {
     const resultsContainer = document.getElementById('recipe-results');
-    resultsContainer.innerHTML = ''; // Clear previous results
+    resultsContainer.innerHTML = '';
 
     for (const recipe of recipes) {
-        // Fetch more details for each recipe
         const detailsUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipe.id}/information`;
+        const detailsOptions = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '6097e393dcmshadee9498b4bc4dbp1772ecjsn591cca02485f',
+                'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+            }
+        };
+
         try {
-            const detailsResponse = await fetch(detailsUrl, {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': 'YOUR_API_KEY',
-                    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-                }
-            });
+            const detailsResponse = await fetch(detailsUrl, detailsOptions);
             const recipeDetails = await detailsResponse.json();
-            // Now you can use recipeDetails to display the full recipe steps and ingredients
             const recipeElem = document.createElement('div');
             recipeElem.classList.add('recipe');
             recipeElem.innerHTML = `
                 <h3>${recipe.title}</h3>
                 <img src="${recipe.image}" alt="${recipe.title}">
-                <p>${recipeDetails.instructions}</p>
-                <ul>${recipeDetails.extendedIngredients.map(ingredient => `<li>${ingredient.original}</li>`).join('')}</ul>
+                <div class="recipe-info">
+                    <p><strong>Preparation time:</strong> ${recipeDetails.readyInMinutes} minutes</p>
+                    <p><strong>Servings:</strong> ${recipeDetails.servings}</p>
+                    <p><strong>Instructions:</strong> ${recipeDetails.instructions}</p>
+                    <h4>Ingredients:</h4>
+                    <ul>${recipeDetails.extendedIngredients.map(ingredient => `<li>${ingredient.original}</li>`).join('')}</ul>
+                </div>
             `;
             resultsContainer.appendChild(recipeElem);
         } catch (error) {
